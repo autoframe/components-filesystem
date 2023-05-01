01896e0a1a9a0d09cb417e0c2771a4cdd6b879cd
Nistor Alexandru Marius <djthorr@gmail.com>
2023-05-02 00:39:08 +0300

    v

---
+++ Tests/Unit/AfrFileVersioningMtimeHashTest.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace Unit;
+
+use Autoframe\Components\FileSystem\Versioning\AfrFileVersioningMtimeHashClass;
+use Autoframe\Components\FileSystem\Versioning\Exception\AfrFileSystemVersioningException;
+use PHPUnit\Framework\TestCase;
+
+class AfrFileVersioningMtimeHashTest extends TestCase
+{
+    function fileVersioningMtimeHashProvider(): array
+    {
+        echo __CLASS__ . '->' . __FUNCTION__ . PHP_EOL;
+        return [
+            [__FILE__, false], //file without error
+            [__FILE__, true], //can throw error if file is missing, but it does
+            ['x', false], //wrong file without error
+            ['x', true],//wrong file that throw error
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider fileVersioningMtimeHashProvider
+     */
+    public function fileVersioningMtimeHashTest(string $sFile, bool $bCanThrow): void
+    {
+        $oClass = new AfrFileVersioningMtimeHashClass();
+        if ($bCanThrow) {
+            if (!is_file($sFile)) {
+                $this->expectException(AfrFileSystemVersioningException::class);
+                $sHash = $oClass->fileVersioningMtimeHash($sFile, true);
+            } else {
+                $sHash = $oClass->fileVersioningMtimeHash($sFile, true);
+                $this->assertEquals(true, strlen($sHash) > 7);
+            }
+
+        } else {
+            $sHash = $oClass->fileVersioningMtimeHash($sFile, false);
+            if ($sHash === '0') {
+                //file was not found and zero was returned as hash without a fatal error
+                $this->assertEquals(true, true);
+            } else {
+                $this->assertEquals(true, strlen($sHash) > 7);
+            }
+        }
+
+    }
+
+
+}
